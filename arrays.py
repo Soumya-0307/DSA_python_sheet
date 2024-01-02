0f2102b53b0d234ea9c2e64688a233a3c172e47b
soumya Hegde <soumyahegde652@gmail.com>
2024-01-02 22:17:14 +0530

    creation of an array

---
+++ arrays.py
@@ -0,0 +1,17 @@
+# creating an array
+
+
+import array
+import numpy as np
+
+
+my_array=array.array('i',[])
+print(my_array)
+my_array1=array.array('i',[1,2,3,4])
+print(my_array1)
+
+np_array=np.array([],dtype=int)
+print(np_array)
+
+np_array1=np.array([11,12,34])
+print(np_array1)
