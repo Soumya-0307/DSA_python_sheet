1c2ce78efe2730b887e1b05c446bde9d73673b15
soumya Hegde <soumyahegde652@gmail.com>
2024-01-02 22:31:55 +0530

    operations perform on array

--- arrays.py
+++ arrays.py
@@ -5,13 +5,28 @@ import array
 import numpy as np
 
 
-my_array=array.array('i',[])
+my_array=array.array('i',[])  #using array_module
 print(my_array)
 my_array1=array.array('i',[1,2,3,4])
 print(my_array1)
 
-np_array=np.array([],dtype=int)
-print(np_array)
 
+np_array=np.array([],dtype=int)  #using numpy module
+print(np_array)
 np_array1=np.array([11,12,34])
 print(np_array1)
+
+
+#insertion of an array using array module
+
+my_array1=array.array('i',[11,12,34,56,78])
+print(my_array1)
+
+my_array1.insert(0,10)
+print(my_array1)
+
+
+
+
+
+
